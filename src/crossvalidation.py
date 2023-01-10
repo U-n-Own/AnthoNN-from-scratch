a7a3de4ae88dea8f0cb803b7bb0f365784684d6d
KamerC1 <80479199+KamerC1@users.noreply.github.com>
2023-01-10 12:27:29 +0100

    Aggiunti controlli per k-fold

--- src/crossvalidation.py
+++ src/crossvalidation.py
@@ -1,13 +1,15 @@
-""" 
+"""
 
 This library will contain our model selection and assessment algorithms.
 
 """
+import math
 
 import numpy as np
 
 
-def _k_fold_partitioning(target_inputs: np.matrix, target_outputs: np.matrix, k: int, fold_index: int) -> (np.matrix, np.matrix, np.matrix, np.matrix):
+def _k_fold_partitioning(target_inputs: np.matrix, target_outputs: np.matrix, k: int, fold_index: int) -> (
+np.matrix, np.matrix, np.matrix, np.matrix):
     """
     K-fold partitioning algorithm
 
@@ -17,22 +19,33 @@ def _k_fold_partitioning(target_inputs: np.matrix, target_outputs: np.matrix, k:
     :param fold_index: index of the fold
     :return: training inputs, training outputs, validation inputs, validation outputs
     """
-    
+
+    if k < 1:
+        raise ValueError(f"k ({k}) must be greater than 0")
+    if k > target_inputs.shape[0]:
+        raise ValueError(f"k ({k}) must be less than the number of samples ({target_inputs.shape[0]})")
+
     len_fold = target_inputs.shape[0] // k
     start = len_fold * fold_index
     end = start + len_fold
-    
+
+    if start >= target_inputs.shape[0]:
+        raise ValueError(f"index out of range (start = {start} >= target_inputs.shape[0] = {target_inputs.shape[0]})")
+    if start < 0:
+        raise ValueError(f"index out of range (start = {start} < 0)")
+
     validation_inputs = target_inputs[start:end]
     validation_outputs = target_outputs[start:end]
-    
+
     training_inputs = np.concatenate((target_inputs[:start], target_inputs[end:]))
     training_outputs = np.concatenate((target_outputs[:start], target_outputs[end:]))
-    
+
     return training_inputs, training_outputs, validation_inputs, validation_outputs
+
 
 def _cross_validation(target_inputs: np.matrix, target_outputs: np.matrix, k: int, model, parameters_set) -> float:
     """
-    Cross validation algorithm using k folds 
+    Cross validation algorithm using k folds
 
     :param target_inputs: matrix containing the inputs
     :param target_outputs: matrix containing the outputs
@@ -41,43 +54,46 @@ def _cross_validation(target_inputs: np.matrix, target_outputs: np.matrix, k: in
     :param kwargs: optional parameters for the model
     :return: the average error
     """
-    
+
     error = 0
     for i in range(k):
-        training_inputs, training_outputs, validation_inputs, validation_outputs = k_fold_partitioning(target_inputs, target_outputs, k, i)
+        training_inputs, training_outputs, validation_inputs, validation_outputs = _k_fold_partitioning(target_inputs,
+                                                                                                       target_outputs,
+                                                                                                       k, i)
         epochs, learning_rate, momentum_term, regularization_term = parameters_set
         model.train(training_inputs, training_outputs, epochs, learning_rate, momentum_term, regularization_term)
-        #TODO Check implementation of validate error function
+        # TODO Check implementation of validate error function
         error += model.validate(validation_inputs, validation_outputs)
-    
+
     return error / k
-    
+
+
 def grid_search(model, parameters_grid: dict, **kwargs) -> dict:
     """
-    Grid search algorithm, GS has complexity O(n^d) 
+    Grid search algorithm, GS has complexity O(n^d)
     where n is the number of parameters and d the number of values for each parameter.
-    Without counting the 
+    Without counting the
 
     :param model: model to use
     :param parameters: dictionary of parameters to test
-    
+
     parameters_grid_example = {
         'learning_rate': [0.1, 0.01, 0.001],
         'momentum_term': [0.1, 0.3, 0.9],
         'regularization_term': [0.1, 0.01, 0.001],
         'epochs': [100, 200, 300]
     }
-    
+
     :param kwargs: optional parameters for the future hyperparameters
     :return: the best parameters
     """
-    
+
     best_parameters = None
     best_error = float('inf')
     for parameters_set in parameters_grid:
-        error = cross_validation(target_inputs, target_outputs, k, model, **parameters_set)
+        error = _cross_validation(target_inputs, target_outputs, k, model, **parameters_set)
         if error < best_error:
             best_error = error
             best_parameters = parameters_set
-            
+
     return best_parameters
