c4cf5d8a1c27ab08c8da37b165871c89044b8881
KamerC1 <80479199+KamerC1@users.noreply.github.com>
2023-01-13 18:28:32 +0100

    Aggiunto model come iper-parametro della grid search

--- src/crossvalidation.py
+++ src/crossvalidation.py
@@ -8,12 +8,13 @@ import itertools
 import numpy as np
 import multiprocessing
 
+from src.NeuralNetwork import NeuralNetwork
 
 def retrain(self, best_model, best_parameters, target_inputs, target_outputs):
     """ Final retrain the best network after cross-validation process """
-    
+
     epochs, learning_rate, momentum_term, regularization_term = best_parameters
-    model.train(target_inputs=target_inputs, target_outputs=target_outputs, epochs=epochs, learning_rate=learning_rate,
+    best_model.train(target_inputs=target_inputs, target_outputs=target_outputs, epochs=epochs, learning_rate=learning_rate,
                 momentum_term = momentum_term, regularization_term=regularization_term)
 
 def _k_fold_partitioning(target_inputs: np.matrix, target_outputs: np.matrix, k: int, fold_index: int) -> (
@@ -25,7 +26,7 @@ np.matrix, np.matrix, np.matrix, np.matrix):
     :param target_outputs: matrix containing the outputs
     :param k: number of folds
     :param fold_index: index of the fold
-    :return: training inputs, training outputs, validation inputs, validation outputs
+    :return: model_selection inputs, model_selection outputs, validation inputs, validation outputs
     """
 
     if k < 1:
@@ -51,7 +52,8 @@ np.matrix, np.matrix, np.matrix, np.matrix):
     return training_inputs, training_outputs, validation_inputs, validation_outputs
 
 
-def _cross_validation(target_inputs: np.matrix, target_outputs: np.matrix, k: int, model, parameters_set, error_queue):
+def _cross_validation(target_inputs: np.matrix, target_outputs: np.matrix, k: int,
+                      model: NeuralNetwork, parameters_set, error_queue):
     """
     Cross validation algorithm using k folds
 
@@ -63,72 +65,86 @@ def _cross_validation(target_inputs: np.matrix, target_outputs: np.matrix, k: in
     :return: the average error
     """
 
+    learning_rate, momentum_term, regularization_term, epochs = parameters_set
+
     error = 0
+
     for i in range(k):
+        print("Fold ", i)
         training_inputs, training_outputs, validation_inputs, validation_outputs = _k_fold_partitioning(target_inputs,
                                                                                                        target_outputs,
                                                                                                        k, i)
-        epochs, learning_rate, momentum_term, regularization_term = parameters_set
-        error_list = model.train(training_inputs, training_outputs, epochs, learning_rate, momentum_term, regularization_term)
-        # TODO Check implementation of validate error function
+
+        model.train(target_inputs = training_inputs, target_outputs = training_outputs,
+                                     epochs=epochs, learning_rate=learning_rate, momentum_term=momentum_term,
+                                     regularization_term=regularization_term)
         error += model.validate(validation_inputs, validation_outputs)
 
     parameters_set = (epochs, learning_rate, momentum_term, regularization_term)
     error_queue.put((error / k, parameters_set))
 
-    #return error / k
 
-
-def grid_search(model, parameters_grid: dict, target_inputs, target_outputs) -> dict:
+def grid_search(parameters_grid: dict, target_inputs: np.matrix, target_outputs: np.matrix, k: int = 5):
     """
     Grid search algorithm, GS has complexity O(n^d)
     where n is the number of parameters and d the number of values for each parameter.
     Without counting the
 
-    :param model: model to use
-    :param parameters: dictionary of parameters to test
+    :param target_inputs: matrice di input dove verrà eseguito il k-fold
+    :param target_outputs: matrice di output dove verrà eseguito il k-fold
+    :param parameters_grid: dictionary of parameters to test
+    :param k: number of folds
 
     parameters_grid_example = {
+        'model': [NeuralNetwork], # model to use
         'learning_rate': [0.1, 0.01, 0.001],
         'momentum_term': [0.1, 0.3, 0.9],
         'regularization_term': [0.1, 0.01, 0.001],
         'epochs': [100, 200, 300]
     }
 
-    :param kwargs: optional parameters for the future hyperparameters
     :return: the best parameters
     """
-    # num folds
-    k = 5
     best_parameters = None
-    best_error = float('inf')
-    parameters_grid = list(itertools.product(*parameters_grid.values()))
-    
+    best_error = None
+
     error_queue = multiprocessing.Queue()
     process_list = []
-    models_error_list = []
-    
-    for parameters_set in parameters_grid:
-        
-        process = multiprocessing.Process(target=_cross_validation, args=(target_inputs, target_outputs, k, model, parameters_set, error_queue))
-        process_list.append(process)
-        
-        #error = _cross_validation(target_inputs, target_outputs, k, model, parameters_set)
-        
+
+    # Remove the model from the parameters grid and create a dictionary with only models
+    models = parameters_grid.pop('model')
+    # Combination of hyperparameters excluding the model
+    combination_grid = list(itertools.product(*parameters_grid.values()))
+
+    for model in models:
+        for parameters_set in combination_grid:
+
+            process = multiprocessing.Process(target=_cross_validation, args=(target_inputs, target_outputs, k, model, parameters_set, error_queue))
+            process_list.append(process)
+
     for process in process_list:
         process.start()
         print("Process {} started".format(process.pid))
     for process in process_list:
         process.join()
         print("Process ", process.pid, " terminated")
-        
+
     # error_queue has a tuple of (error, parameters_set)
     # extract best error and best_parameters
+    error_list = []
+    parameters_list = []
     while not error_queue.empty():
         error = error_queue.get()
-        if error[0] < best_error:
+        error_list.append(error[0])
+        parameters_list.append(error[1])
+
+        if best_error is None or error[0] < best_error:
             best_error = error[0]
             best_parameters = error[1]
-    
-    return best_parameters, best_error
-        
+
+    return {
+        'best_error': best_error,
+        'best_parameters': best_parameters,
+        'error_list': error_list,
+        'parameters_list': parameters_list
+    }
